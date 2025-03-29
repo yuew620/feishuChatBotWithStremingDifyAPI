72d60274b4b0d63b6ff3ec0f1621feee07c6099a
Wang Yue <yyuewang@amazon.com>
2025-03-29 09:29:28 +0800

    test

--- code/handlers/init.go
+++ code/handlers/init.go
@@ -6,7 +6,6 @@ import (
 	"start-feishubot/initialization"
 	"start-feishubot/services/accesscontrol"
 	"start-feishubot/services/ai"
-	"start-feishubot/services/cardcreator"
 	"start-feishubot/services/cardservice"
 )
 
@@ -14,20 +13,23 @@ var handler MessageHandlerInterface
 
 func InitHandlers(config initialization.Config) error {
 	// 初始化AI提供商
-	provider, err := ai.InitAIProvider()
+	provider, err := ai.InitAIProvider(config)
 	if err != nil {
 		return err
 	}
 
 	// 初始化访问控制
 	if config.AccessControlEnable {
-		accesscontrol.InitAccessControl()
+		err := accesscontrol.InitAccessControl(config)
+		if err != nil {
+			return err
+		}
 	}
 
 	// 初始化卡片池
-	cardservice.InitCardPool(func(ctx context.Context, content string) (string, error) {
+	cardservice.InitCardPool(cardservice.CreateCardFn(func(ctx context.Context, content string) (string, error) {
 		return CreateCardEntity(ctx, content)
-	})
+	}))
 
 	// 创建消息处理器
 	h, err := NewMessageHandler(config)
