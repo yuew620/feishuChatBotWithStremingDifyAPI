eea3eb5479bf7a1416106f7dab6a7fa7fadabba9
Wang Yue <yyuewang@amazon.com>
2025-03-31 09:40:10 +0800

    test

--- code/initialization/services.go
+++ code/initialization/services.go
@@ -1,7 +1,10 @@
 package initialization
 
 import (
+	"start-feishubot/services"
+	"start-feishubot/services/cardcreator"
 	"start-feishubot/services/core"
+	"start-feishubot/services/feishu"
 )
 
 var (
@@ -10,9 +13,38 @@ var (
 	msgCache     core.MessageCache
 )
 
+// NewMessageCache creates a new message cache
+func NewMessageCache() core.MessageCache {
+	return core.NewMessageCache()
+}
+
+// NewSessionCache creates a new session cache
+func NewSessionCache() core.SessionCache {
+	return services.GetSessionCache()
+}
+
 // InitializeServices initializes all services
 func InitializeServices() error {
-	// Initialize services here
+	// Get config
+	config := GetConfig()
+
+	// Initialize Feishu config adapter
+	feishuConfig := feishu.NewConfigAdapter(config)
+
+	// Initialize card creator
+	cardCreator = cardcreator.NewCardCreator(feishuConfig)
+
+	// Initialize card pool
+	if err := InitCardPool(cardCreator.CreateCardEntity); err != nil {
+		return err
+	}
+
+	// Initialize session cache
+	sessionCache = NewSessionCache()
+
+	// Initialize message cache
+	msgCache = NewMessageCache()
+
 	return nil
 }
 
